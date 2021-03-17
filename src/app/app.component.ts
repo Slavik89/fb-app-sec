e207bb0939d2ee6b23fdbeff4c373c86e4394705
Slavik89 <terakrokos@gmail.com>
2021-03-17 13:33:01 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireDatabase} from '@angular/fire/database';
+import { Observable } from 'rxjs';
+import { CrudService } from './crud.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fb-app-sec';
+  
+
+  courses: any[];
+
+  // todo = this.store.collection('todo').valueChanges({ idField: 'id' })
+
+  items: Observable<any[]>;
+  constructor(public crudService: CrudService) {
+    // this.items = db.collection('names').snapshotChanges();
+  }
+
+  ngOnInit() {
+    this.crudService.getAllNAmes().subscribe(data =>{
+      this.courses = data.map(e => e.payload.doc.data()['1']);
+      // this.courses = data;
+      console.log(this.courses);
+    });
+  }
+
 }
